8972f597b6e1a52a0a893ca3723073721ba141b3
ting <ttzhenng@gmail.com>
2020-11-26 14:53:49 -0500

    commit tree ef5945887601a408d09a7548bef89982cabedd1c

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  if array.count < 2
+    return r = array.join(", "){ |e| "#{e}"}
+  elsif array.count == 2
+    #r = array.delete_at(array.index(","))
+    #return r.join()
+    return r = array.join(" and "){ |e| "#{e}"}
+  else
+    r = array.insert((array.count - 1),"and ")
+    r = r.map {|x| [x, ", "]}.flatten
+        #r = r.map {|x| [x, ","]}.flatten
+    r.pop()
+    r.delete_at(-2)
+    r.delete_at(r.length())
+    return r.join()
+    end
 end
